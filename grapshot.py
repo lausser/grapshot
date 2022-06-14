ac7f8951df8970f44ef285417f0fa399485d3446
Gerhard Lausser <gerhard.lausser@consol.de>
2022-06-14 15:40:53 +0200

    scroll faster

--- grapshot.py
+++ grapshot.py
@@ -153,8 +153,8 @@ async def main(config):
 
                 await loc_div_dashboard_scroll.hover()
                 while div_react_grid_layout["height"]+div_react_grid_layout["y"] >= div_dashboard_container["height"]:
-                    logging.debug("scroll down {} pixel...".format(int(viewport_height/3)))
-                    await page.mouse.wheel(0, int(viewport_height/3))
+                    logging.debug("scroll down {} pixel...".format(int(viewport_height/2)))
+                    await page.mouse.wheel(0, int(viewport_height/2))
                     try:
                         await expect(loc_loading_wheel).to_have_count(0, timeout=load_wait)
                         logging.debug("no loading wheel found")
